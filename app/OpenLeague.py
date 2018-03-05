e3edd004a69319e59245ebef975e65446e08cc0b
FevenKitsune <fevenkitsune@gmail.com>
2018-03-05 18:13:27 +0000

    Added imports, header, and a basic hello world.

--- app/OpenLeague.py
+++ app/OpenLeague.py
@@ -1,1 +1,14 @@
-# Syntax Guide: https://www.python.org/dev/peps/pep-0008
+"""Program: OpenLeague
+Author: Feven Kitsune <email upon request>
+License: GNU GENERAL PUBLIC LICENSE v3.0
+
+Author Notes:
+Syntax Guide: https://www.python.org/dev/peps/pep-0008
+"""
+
+import discord
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+print("Hello world!")
