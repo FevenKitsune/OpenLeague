6e374a52eaa24b294c4d61675c214831eb79e818
FevenKitsune <fevenkitsune@gmail.com>
2018-03-13 21:42:10 +0000

    Updated the formatting for the example extension. More extensions to come soon.

--- app/ExtensionExample.py
+++ app/ExtensionExample.py
@@ -8,21 +8,29 @@ import discord
 import logging
 from discord.ext import commands
 
-# Extension logger.
+# Extension logger (Unused currently)
 ext = logging.getLogger("ext")
 
 class ExtensionExample():
-    def __init__(self,client):
+
+    """INITIALIZER
+    What is first called when the class ExtensionExample() is loaded. Set's the class's client variable to the client passed by the main client.
+    """
+
+    def __init__(self, client):
         self.client = client
-    
+
     """EXTENSION
-    An example of an extension!
+    An example of an extension command!
     See https://gist.github.com/leovoel/46cd89ed6a8f41fd09c5 for a useful reference about extensions.
     """
-    
-    @commands.command(name='extension', aliases=['ext'], brief='Extension example', description='An example of a loaded extension!', usage='<none>')
+
+    @commands.command(name='extension', aliases=['ext'], brief='Extension example', description='An example of a loaded extension!', usage='')
     async def extension(self, ctx, *args):
         await ctx.send(":ok_hand: Extension loaded!")
-        
+
+"""SETUP
+Called by DiscordExtensions. Loads the extension into the client.
+"""
 def setup(client):
     client.add_cog(ExtensionExample(client))
