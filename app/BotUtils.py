1ce7e13c8ec1075fe1305824b134de6d85d49e3b
FevenKitsune <fevenkitsune@gmail.com>
2018-04-14 22:07:09 +0000

    Cleaned up code. Still struggling with code format consistency. I really need to study a style guide.

--- app/BotUtils.py
+++ app/BotUtils.py
@@ -41,6 +41,7 @@ class BotUtils():
     
     @commands.command(name='membercount', aliases=['mc','memcount','mcount'], brief='Server Member Count', description='Returns the number of members in the server.', usage='<none>')
     async def membercount(self, ctx, *args):
+    
         if ctx.guild:
             await ctx.send(":eyes: I see " + str(len(ctx.guild.members)) + " members in this server.")
         else:
@@ -53,30 +54,41 @@ class BotUtils():
 
     @commands.command(name='randomint', aliases=['rn','randint','rint'], brief='Random Integer Generator', description='Generate an integer from <n1> to <n2>', usage='<n1> <n2>')
     async def randomint(self, ctx, *args):
+    
+        # No numbers passed.
         if len(args) == 0:
             await ctx.send(":negative_squared_cross_mark: Not enough arguments!")
             return
-            
-        elif len(args) == 1:
-            if isinstance(int(args[0]), int):
-                await ctx.send(":game_die: " + str(randint(0, int(args[0]))))
-                return
-            else:
+        
+        # One number passed.
+        if len(args) == 1:
+        
+            # Checks if number is an integer.
+            if not isinstance(int(args[0]), int):
                 await ctx.send(":negative_squared_cross_mark: Invalid arguments!")
                 return
                 
-        elif len(args) == 2:
-            if isinstance(int(args[0]), int) and isinstance(int(args[1]), int):
-                if int(args[0]) < int(args[1]):
-                    await ctx.send(":game_die: " + str(randint(int(args[0]), int(args[1]))))
-                    return
-                else:
-                    await ctx.send(":negative_squared_cross_mark: Invalid arguments!")
-                    return
-                    
-            else:
+            await ctx.send(":game_die: " + str(randint(0, int(args[0]))))
+            return
+
+        # Two numbers passed.
+        if len(args) == 2:
+        
+            # Checks if both numbers are integers.
+            if not isinstance(int(args[0]), int) or
+            not isinstance(int(args[1]), int):
                 await ctx.send(":negative_squared_cross_mark: Invalid arguments!")
                 return
+            
+            # Ensures the first number given is smaller than the second.
+            if not (int(args[0]) < int(args[1])):
+                await ctx.send(":negative_squared_cross_mark: Invalid arguments!")
+                return
+                
+            await ctx.send(":game_die: " + str(randint(int(args[0]), int(args[1]))))
+            return
+
+        # More than two numbers passed.
         else:
             await ctx.send(":negative_squared_cross_mark: Too many arguments!")
             return
