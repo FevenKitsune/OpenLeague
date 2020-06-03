54d5ab1e87035a03e2e0fbbe3b8c2b59d37f51e9
Feven Kitsune <fevenkitsune@gmail.com>
2020-06-03 15:41:09 -0400

    Update OpenLeague.py
    + change license

--- app/OpenLeague.py
+++ app/OpenLeague.py
@@ -1,7 +1,7 @@
 """MAIN
 Program: OpenLeague
 Author: Feven Kitsune <email upon request>
-License: GNU GENERAL PUBLIC LICENSE v3.0
+License: CC0 1.0 Universal
 
 Author Notes:
 Syntax Guide: https://www.python.org/dev/peps/pep-0008
